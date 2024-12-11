9ede2ec512fce1f220bceb7c73a1d51a7b06d734
Tarek Demachkie <tarek.demachkie@getslingshot.com>
2024-12-11 00:54:46 -0600

    fix migrations

---
+++ migrations/1733900003144-InitialMigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialMigration1733900003144 implements MigrationInterface {
+    name = 'InitialMigration1733900003144'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "email" character varying NOT NULL, "subscriptionType" "public"."user_subscriptiontype_enum" NOT NULL DEFAULT 'free', "accountId" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "todayAiUsage" integer NOT NULL DEFAULT '0', "todayAiUsageTimestamp" TIMESTAMP NOT NULL DEFAULT '"2024-12-11T06:53:24.977Z"', CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
--- typeOrm.config.ts
+++ typeOrm.config.ts
@@ -2,9 +2,7 @@ import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv-flow';
 import { User } from './src/users/entities/user.entity';
-import { AddUserEntity1730581088273 } from './migrations/1730581088273-addUserEntity';
-import { AddUserDateColumns1730618461014 } from './migrations/1730618461014-addUserDateColumns';
-import { AddUserDateAndAiLimitColumns1731312429016 } from './migrations/1731312429016-AddUserDateAndAiLimitColumns';
+import { InitialMigration1733900003144 } from './migrations/1733900003144-InitialMigration';
 
 dotenv.config();
 
@@ -18,9 +16,5 @@ export default new DataSource({
   },
   url: process.env.DATABASE_URL,
   entities: [User],
-  migrations: [
-    AddUserEntity1730581088273,
-    AddUserDateColumns1730618461014,
-    AddUserDateAndAiLimitColumns1731312429016,
-  ],
+  migrations: [InitialMigration1733900003144],
 });
